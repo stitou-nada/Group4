2f7afad627fbc77cc9d7dbd593d7fd7bf4687fbe
alaeSouihli1 <alaemar2018@gmail.com>
2022-10-05 10:10:49 +0100

    first

---
+++ Prototypes/script2.js
@@ -0,0 +1,5 @@
+const piano=document.querySelector('.piano')
+// piano.insertAdjacentHTML('beforeend',html)
+const notes=document.querySelectorAll('.cr')
+const keys=["A","B","C","D"]
+document.addEventListener("keydown",(e))
