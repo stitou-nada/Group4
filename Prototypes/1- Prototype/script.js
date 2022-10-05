94b5bfb985c321449df6ccf7ed8bbcb0d77e2954
stitou-nada <nadastito1087@gmail.com>
2022-10-05 15:48:32 +0100

    prototype 1

--- Prototypes/1- Prototype/script.js
+++ Prototypes/1- Prototype/script.js
@@ -1,32 +1,26 @@
-const keys = document.querySelectorAll(".key"),
-  note = document.querySelector(".nowplaying"),
-  hints = document.querySelectorAll(".hints");
-
-function playNote(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`),
-    key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-
-  if (!key) return;
-
-  const keyNote = key.getAttribute("data-note");
-
-  key.classList.add("playing");
-  note.innerHTML = keyNote;
-  audio.currentTime = 0;
-  audio.play();
+function timer(){
+	var StartTime= new Date();
+				function DisplayTime(){
+					var CurrentTime= new Date();
+					var TimeDiff=CurrentTime.getTime()-StartTime.getTime();
+					TimeDiff=Math.floor(TimeDiff/1000);
+			        var ShowTime=(TimeDiff<10)?'0:0':'0:';
+				    document.getElementById('timerwatch').innerHTML=ShowTime+TimeDiff;
+				}
+				var StopTime=setInterval(DisplayTime, 1000);
+				function StopUpdatingTime(){
+					clearInterval(StopTime);
+					document.getElementById("inputbox").disabled = true;
+				}
+				setTimeout(StopUpdatingTime, 60000);
 }
-
-function removeTransition(e) {
-  if (e.propertyName !== "transform") return;
-  this.classList.remove("playing");
+function speedCal(){
+		function Speed(){
+		var inputText=document.getElementById("inputbox").value;
+		inputText = inputText.replace(/\s\s+/g, ' ');
+		inputText=inputText.trim();
+		var arr=inputText.split(" ");
+		document.getElementById('timerwatch').innerHTML="Your Speed is "+arr.length+" Words/S.";
+		}
+    	setTimeout(Speed, 61000);
 }
-
-function hintsOn(e, index) {
-  e.setAttribute("style", "transition-delay:" + index * 50 + "ms");
-}
-
-hints.forEach(hintsOn);
-
-keys.forEach(key => key.addEventListener("transitionend", removeTransition));
-
-window.addEventListener("keydown", playNote);
