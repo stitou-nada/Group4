37b59956e1af2ae84588d9c562f088bc199f7c24
alaeSouihli1 <alaemar2018@gmail.com>
2022-10-06 09:45:56 +0100

    add

--- Prototypes/script2.js
+++ Prototypes/script2.js
@@ -3,16 +3,25 @@
 // const notes=document.querySelectorAll('.cr')
 // const keys=["A","B","C","D"]
 
-var audioA=new Audio('./../notes/A.mp3')
-var audioB=new Audio('./../notes/B.mp3')
-var audioC=new Audio('./../notes/C.mp3')
-var audioD=new Audio('./../notes/D.mp3')
+var audioA=new Audio('https://awiclass.monoame.com/pianosound/set/1.wav')
+var audioB=new Audio('https://awiclass.monoame.com/pianosound/set/2.wav')
+var audioC=new Audio('https://awiclass.monoame.com/pianosound/set/3.wav')
+var audioD=new Audio('https://awiclass.monoame.com/pianosound/set/4.wav')
+var audioE=new Audio('https://awiclass.monoame.com/pianosound/set/5.wav')
+var audioF=new Audio('https://awiclass.monoame.com/pianosound/set/6.wav')
+var audioG=new Audio('https://awiclass.monoame.com/pianosound/set/7.wav')
+var audioH=new Audio('https://awiclass.monoame.com/pianosound/set/8.wav')
+
+
 
 const key1='A'
 document.addEventListener('keydown',(e)=>{
     if(e.key==key1  || e.key==key1.toLowerCase() ){
         audioA.play()
+    
         document.getElementById("A").style.color="red"
+
+
 
     }
 
@@ -20,7 +29,8 @@ document.addEventListener('keydown',(e)=>{
 const key2='B'
 document.addEventListener('keydown',(e)=>{
     if(e.key==key2 || e.key==key2.toLowerCase()){
-        audioB.play()
+        // audioB.play()
+        audioA.play()
         document.getElementById("B").style.color="red"
 
     }
@@ -28,7 +38,8 @@ document.addEventListener('keydown',(e)=>{
 const key3='C'
 document.addEventListener('keydown',(e)=>{
     if(e.key==key3  || e.key==key3.toLowerCase()){
-        audioC.play()
+        // audioC.play()
+        audioB.play()
         document.getElementById("C").style.color="red"
 
     }
@@ -37,8 +48,60 @@ document.addEventListener('keydown',(e)=>{
 const key4='D'
 document.addEventListener('keydown',(e)=>{
     if(e.key==key4  || e.key==key4.toLowerCase()){
-        audioD.play()
+        // audioD.play()
+        audioA.play()
         document.getElementById("D").style.color="red"
+
+
+
+
+    }})
+
+const key5='E'
+document.addEventListener('keydown',(e)=>{
+    if(e.key==key5 || e.key==key5.toLowerCase()){
+        // audioE.play()
+        audioD.play()
+        document.getElementById("E").style.color="red"
+    
+    
+    
+    
+        }
+    
+    })
+    const key6='F'
+document.addEventListener('keydown',(e)=>{
+    if(e.key==key6  || e.key==key6.toLowerCase()){
+        // audioF.play()
+        audioC.play()
+        document.getElementById("F").style.color="red"
+
+
+
+
+    }
+
+})
+const key7='G'
+document.addEventListener('keydown',(e)=>{
+    if(e.key==key7  || e.key==key7.toLowerCase()){
+        audioG.play()
+        document.getElementById("G").style.color="red"
+
+
+
+
+    }
+
+})
+const key8='H'
+document.addEventListener('keydown',(e)=>{
+    if(e.key==key8  || e.key==key8.toLowerCase()){
+        audioH.play()
+        document.getElementById("H").style.color="red"
+
+
 
 
     }
